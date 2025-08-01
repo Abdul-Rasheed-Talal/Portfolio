404c67590ccaf08c44e8f88c8b44fb6812077316
Abdul Rasheed Talal <mabdulrasheedtalal@gmail.com>
2025-08-01 14:41:33 +0500

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -61,11 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "127.0.0.1",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+ app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
 })();
